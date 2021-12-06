2cd1c2a076b7a5dafcc012de113f4014830a5422
kseniaGoloveshkina <91425084+kseniaGoloveshkina@users.noreply.github.com>
2021-12-06 14:56:06 +0300

    Add files via upload
    ВВП 6лаба, все задания

---
+++ 6лаб0.py
@@ -0,0 +1,56 @@
+print('введите первое число')
+a= int(input())
+print('введите второе число')
+b= int(input())
+a,b = b,a
+print(a)
+print(b)
+
+print('введите A')
+a= int(input())
+print('введите B')
+b= int(input())
+print('введите C')
+c= int(input())
+a,b,c = b,c,a
+print(a)
+print(b)
+print(c)
+
+print('введите A')
+a= int(input())
+print('введите B')
+b= int(input())
+print('введите C')
+c= int(input())
+a,b,c = c,a,b
+print(a)
+print(b)
+print(c)
+
+print('введите x')
+x= int(input())
+y= ((3*x**6)-(6*x**2)-7)
+print('Уравнение=' ,y)
+
+print('введите x')
+x= int(input())
+y= (4*((x-3)**6))-(7*((x-3)**3))+2
+print('Уравнение=' ,y)
+
+print('Введите A')
+a= int(input())
+b = a*a
+a = b*b
+b = a*a
+print(b)
+
+
+print('Введите A')
+a= int(input())
+b = a*a
+a = b*a
+b = a*a
+a = b*a
+b = a*b
+print(b)
